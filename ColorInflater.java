4733fc1cf730147748fbd382ffb22f5542469876
jdpennell <seanm@activewebsite.com>
2015-11-18 21:49:03 -0700

    Create ColorInflater.java

---
+++ ColorInflater.java
@@ -0,0 +1,507 @@
+package com.example.app;
+
+import android.support.v4.content.ContextCompat;
+
+import com.example.app.R;
+import com.example.app.MainActivity;
+
+/**
+ * Android would prefer we use ContextCompat, rather than resource IDs,
+ * to retrieve from values/colors.xml. This is a thin wrapper which
+ * accomplishes this.
+ *
+ * Please see colors.xml for an easier to read list of available colors.
+ *
+ * @author Jessica Pennell &lt;jessicadharmapennell@gmail.com&gt;
+ */
+public class ColorInflater
+{
+  public static int Clear()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Clear);
+  }
+  public static int Mahogany()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Mahogany);
+  }
+  public static int FuzzyWuzzyBrown()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.FuzzyWuzzyBrown);
+  }
+  public static int Chestnut()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Chestnut);
+  }
+  public static int RedOrange()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.RedOrange);
+  }
+  public static int SunsetOrange()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.SunsetOrange);
+  }
+  public static int Bittersweet()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Bittersweet);
+  }
+  public static int Melon()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Melon);
+  }
+  public static int OutrageousOrange()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.OutrageousOrange);
+  }
+  public static int VividTangerine()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.VividTangerine);
+  }
+  public static int BurntSienna()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.BurntSienna);
+  }
+  public static int Brown()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Brown);
+  }
+  public static int Sepia()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Sepia);
+  }
+  public static int Orange()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Orange);
+  }
+  public static int BurntOrange()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.BurntOrange);
+  }
+  public static int Copper()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Copper);
+  }
+  public static int MangoTango()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.MangoTango);
+  }
+  public static int AtomicTangerine()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.AtomicTangerine);
+  }
+  public static int Beaver()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Beaver);
+  }
+  public static int AntiqueBrass()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.AntiqueBrass);
+  }
+  public static int DesertSand()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.DesertSand);
+  }
+  public static int RawSienna()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.RawSienna);
+  }
+  public static int Tumbleweed()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Tumbleweed);
+  }
+  public static int Tan()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Tan);
+  }
+  public static int Peach()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Peach);
+  }
+  public static int MacaroniAndCheese()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.MacaroniAndCheese);
+  }
+  public static int Apricot()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Apricot);
+  }
+  public static int NeonCarrot()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.NeonCarrot);
+  }
+  public static int Almond()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Almond);
+  }
+  public static int YellowOrange()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.YellowOrange);
+  }
+  public static int Gold()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Gold);
+  }
+  public static int Shadow()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Shadow);
+  }
+  public static int BananaMania()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.BananaMania);
+  }
+  public static int Sunglow()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Sunglow);
+  }
+  public static int Goldenrod()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Goldenrod);
+  }
+  public static int Dandelion()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Dandelion);
+  }
+  public static int Yellow()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Yellow);
+  }
+  public static int GreenYellow()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.GreenYellow);
+  }
+  public static int SpringGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.SpringGreen);
+  }
+  public static int OliveGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.OliveGreen);
+  }
+  public static int LaserLemon()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.LaserLemon);
+  }
+  public static int UnmellowYellow()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.UnmellowYellow);
+  }
+  public static int Canary()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Canary);
+  }
+  public static int YellowGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.YellowGreen);
+  }
+  public static int InchWorm()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.InchWorm);
+  }
+  public static int Asparagus()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Asparagus);
+  }
+  public static int GrannySmithApple()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.GrannySmithApple);
+  }
+  public static int ElectricLime()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.ElectricLime);
+  }
+  public static int ScreaminGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.ScreaminGreen);
+  }
+  public static int Fern()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Fern);
+  }
+  public static int ForestGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.ForestGreen);
+  }
+  public static int SeaGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.SeaGreen);
+  }
+  public static int Green()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Green);
+  }
+  public static int MountainMeadow()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.MountainMeadow);
+  }
+  public static int Shamrock()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Shamrock);
+  }
+  public static int JungleGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.JungleGreen);
+  }
+  public static int CaribbeanGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.CaribbeanGreen);
+  }
+  public static int TropicalRainForest()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.TropicalRainForest);
+  }
+  public static int PineGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PineGreen);
+  }
+  public static int RobinEggBlue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.RobinEggBlue);
+  }
+  public static int Aquamarine()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Aquamarine);
+  }
+  public static int TurquoiseBlue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.TurquoiseBlue);
+  }
+  public static int SkyBlue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.SkyBlue);
+  }
+  public static int OuterSpace()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.OuterSpace);
+  }
+  public static int BlueGreen()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.BlueGreen);
+  }
+  public static int PacificBlue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PacificBlue);
+  }
+  public static int Cerulean()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Cerulean);
+  }
+  public static int Cornflower()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Cornflower);
+  }
+  public static int MidnightBlue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.MidnightBlue);
+  }
+  public static int NavyBlue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.NavyBlue);
+  }
+  public static int Denim()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Denim);
+  }
+  public static int Blue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Blue);
+  }
+  public static int Periwinkle()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Periwinkle);
+  }
+  public static int CadetBlue()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.CadetBlue);
+  }
+  public static int Indigo()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Indigo);
+  }
+  public static int WildBlueYonder()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.WildBlueYonder);
+  }
+  public static int Manatee()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Manatee);
+  }
+  public static int BlueBell()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.BlueBell);
+  }
+  public static int BlueViolet()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.BlueViolet);
+  }
+  public static int PurpleHeart()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PurpleHeart);
+  }
+  public static int RoyalPurple()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.RoyalPurple);
+  }
+  public static int PurpleMountainsMajesty()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PurpleMountainsMajesty);
+  }
+  public static int Violet()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Violet);
+  }
+  public static int Purple()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Purple);
+  }
+  public static int Wisteria()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Wisteria);
+  }
+  public static int VividViolet()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.VividViolet);
+  }
+  public static int Fuchsia()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Fuchsia);
+  }
+  public static int ShockingPink()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.ShockingPink);
+  }
+  public static int PinkFlamingo()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PinkFlamingo);
+  }
+  public static int Plum()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Plum);
+  }
+  public static int HotMagenta()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.HotMagenta);
+  }
+  public static int PurplePizzazz()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PurplePizzazz);
+  }
+  public static int RazzleDazzleRose()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.RazzleDazzleRose);
+  }
+  public static int Orchid()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Orchid);
+  }
+  public static int RedViolet()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.RedViolet);
+  }
+  public static int Eggplant()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Eggplant);
+  }
+  public static int Cerise()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Cerise);
+  }
+  public static int WildStrawberry()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.WildStrawberry);
+  }
+  public static int Magenta()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Magenta);
+  }
+  public static int Lavender()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Lavender);
+  }
+  public static int CottonCandy()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.CottonCandy);
+  }
+  public static int VioletRed()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.VioletRed);
+  }
+  public static int CarnationPink()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.CarnationPink);
+  }
+  public static int Razzmatazz()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Razzmatazz);
+  }
+  public static int PiggyPink()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PiggyPink);
+  }
+  public static int JazzberryJam()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.JazzberryJam);
+  }
+  public static int Blush()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Blush);
+  }
+  public static int TickleMePink()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.TickleMePink);
+  }
+  public static int PinkSherbet()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.PinkSherbet);
+  }
+  public static int Maroon()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Maroon);
+  }
+  public static int Red()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Red);
+  }
+  public static int RadicalRed()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.RadicalRed);
+  }
+  public static int Mauvelous()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Mauvelous);
+  }
+  public static int WildWatermelon()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.WildWatermelon);
+  }
+  public static int Scarlet()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Scarlet);
+  }
+  public static int Salmon()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Salmon);
+  }
+  public static int BrickRed()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.BrickRed);
+  }
+  public static int White()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.White);
+  }
+  public static int Timberwolf()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Timberwolf);
+  }
+  public static int Silver()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Silver);
+  }
+  public static int Gray()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Gray);
+  }
+  public static int Black()
+  {
+    return ContextCompat.getColor(MainActivity.mainActivity, R.color.Black);
+  }
+}
